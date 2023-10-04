266cc4c1c6de50219e3d29d228bd3fdf45e3d5d1
unknown <bharathi12.cse@gmail.com>
2023-10-04 12:38:05 +0530

    wseew

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KFCcomponentComponent } from './Logout/kfccomponent/kfccomponent.component';
+import { ChickencomponentComponent } from './loggedin/chickencomponent/chickencomponent.component';
+import { BurgerscomponentComponent } from './loggedin/burgerscomponent/burgerscomponent.component';
+import { BoxmealscomponentComponent } from './loggedin/boxmealscomponent/boxmealscomponent.component';
+import { SnakscomponentComponent } from './loggedin/snakscomponent/snakscomponent.component';
+import { BeveragescomponentComponent } from './loggedin/beveragescomponent/beveragescomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'home', component: KFCcomponentComponent },
+
+  { path: 'chicken', component: ChickencomponentComponent },
+
+  { path: 'burger', component: BurgerscomponentComponent },
+
+  { path: 'boxmeals', component: BoxmealscomponentComponent },
+
+  { path: 'snaks', component: SnakscomponentComponent },
+
+  { path: 'beverages', component: BeveragescomponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
